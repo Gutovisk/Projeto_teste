09bb0f8d0bdb543d534dc333cfb2f54d1e4180b9
Gutovisk <augustoguerreiro@yahoo.com>
2021-04-16 15:50:36 -0300

    Alteracao na função batata

--- Cena/Cena.gd
+++ Cena/Cena.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+var vida = 100
+var speed = 34
+
+
 func _ready() -> void:
 	print("Hehe")
 	Augusto()
 
 func professorRicardo():
-	batata()
+	Davi()
 	print("Ricardo")
 
-func batata():
+func Davi():
 	Augusto()
 	print("Meu nome é Davi, eu acho...")
 
